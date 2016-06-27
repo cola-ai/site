101465204bf9d59db569716d52cfb36f1d3203a5
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-06-27 17:15:45 -0300

    refactor(spring): renomeado groupid e packages do projeto

---
+++ src/cwi-colaai/cwi-colaai-service/src/main/java/br/com/cwi/colaai/service/servicos/ServicoUsuario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cwi.colaai.service.servicos;
+
+import br.com.colaai.view_model.UsuarioViewModel;
+
+/**
+ *
+ * @author erico.loewe
+ */
+public class ServicoUsuario {
+
+    public void criar(UsuarioViewModel usuario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/cwi-colaai/cwi-colaai-web/src/main/java/br/com/cwi/colaai/web/controllers/AcessController.java
@@ -0,0 +1,40 @@
+package br.com.cwi.colaai.web.controllers;
+
+import br.com.colaai.servicos.ServicoUsuario;
+import br.com.colaai.view_model.UsuarioViewModel;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Carlos H. Nonnemacher
+ */
+@Controller
+public class AcessController {
+
+    @Autowired
+    private ServicoUsuario _servicoUsuario;
+    
+    @RequestMapping(value="/login")
+    public String login() {
+        return "login";
+    } 
+    
+    @RequestMapping(value="/logout")
+    public String logout(HttpSession httpSession) {
+        httpSession.invalidate();
+        return "redirect:login";
+    } 
+    
+    @RequestMapping(value="/cadastrar")
+    public String cadastrar() {
+        return "cadastrar";
+    }
+    
+    @RequestMapping(value="/salvar")
+    public String cadastrar(UsuarioViewModel usuario) {
+        _servicoUsuario.criar(usuario);
+        return "login";
+    }
+}
