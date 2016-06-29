7cdc5747e195784eeab9e184ca7071d980d1e73c
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-06-28 21:10:29 -0300

    feat(usuario): começado a implementar validações.

--- src/cwi-colaai/cwi-colaai-web/src/main/resources/static/js/site/view/usuarioView.js
+++ src/cwi-colaai/cwi-colaai-web/src/main/resources/static/js/site/view/usuarioView.js
@@ -1,23 +1,54 @@
 "use strict";
 (function () {
     window.App = window.App || {};
-    
+
     App.CadastroUsuarioView = {
-        iniciar: function() {
+        iniciar: function () {
             this.buscarElementos();
             this.vincularEventos();
         },
-        
-        buscarElementos: function() {
+        buscarElementos: function () {
             this.$form = $(".cadastrar-form");
         },
-        
-        vincularEventos: function() {
-            this.$form.submit(function() {
-                
+        vincularEventos: function () {
+            this.$form.validate({
+                rules: {
+                    nome: {
+                        required: true,
+                        minlength: 3
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    senha: {
+                        required: true
+                    },
+                    confirmarSenha: {
+                        required: true,
+                        equalTo: "#senha"
+                    }
+                },
+                messages: {
+                    nome: {
+                        required: "O campo nome é obrigatório.",
+                        minlength: "O campo nome deve conter no mínimo 3 caracteres."
+                    },
+                    email: {
+                        required: "O campo email é obrigatório.",
+                        email: "O campo email deve conter um email válido."
+                    },
+                    senha: {
+                        required: "O campo senha é obrigatório."
+                    },
+                    confirmarSenha: {
+                        required: "O campo confirmação de senha é obrigatório.",
+                        equalTo: "O campo confirmação de senha deve ser identico ao campo senha."
+                    }
+                }
             });
         }
     };
-    
+
     App.CadastroUsuarioView.iniciar();
 })();
