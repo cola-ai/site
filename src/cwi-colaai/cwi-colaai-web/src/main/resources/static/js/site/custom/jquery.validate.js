8505254a67bc9a36ecb79c06339fa7d8e1fb9250
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-06-29 09:53:17 -0300

    feat(Usuario): terminado validações no cadastro e login do usuario.

---
+++ src/cwi-colaai/cwi-colaai-web/src/main/resources/static/js/site/custom/jquery.validate.js
@@ -0,0 +1,84 @@
+/*
+ * jquery validate
+ */
+"use strict";
+(function () {
+    window.App = window.App || {};
+
+    App.Validador = {
+        iniciar: function () {
+            this.criarMetodos();
+            this.criarRules();            
+        },
+        
+        criarMetodos: function () {
+            $.validator.addMethod("password_check", function (value) {
+                return /^[A-Za-z0-9\d=!\-@._*]*$/.test(value) // caracter especial
+                        && /[a-z]/.test(value) // tem uma letra minuscula
+                        && /\d/.test(value); // tem um digito
+            }, function(params, element) {
+                return !(/^[A-Za-z0-9\d=!\-@._*]*$/.test($(element).val())) ? "Sua senha deve conter um 1 caracter especial" : 
+                        !(/[a-z]/.test($(element).val())) ? "Sua senha deve conter uma letra minuscula" :
+                        "Sua senha deve conter um digito";
+            });
+        },
+        
+        criarRules: function () {
+            jQuery.validator.addClassRules({
+                "validar-nome": {
+                    required: true,
+                    minlength: 3
+                },
+                "validar-sobrenome": {
+                    required: true,
+                    minlength: 3
+                },
+                "validar-telefone": {
+                    required: true,
+                    minlength: 16,
+                    maxlength: 17
+                },
+                "validar-sexo": {
+                    required: true
+                },
+                "validar-email": {
+                    required: true,
+                    email: true
+                },
+                "validar-senha": {
+                    required: true,
+                    minlength: 6,
+                    password_check: true
+                },
+                "validar-confirmarSenha": {
+                    required: true,
+                    equalTo: "#senha"
+                },
+                "validar-arquivo": {
+                    required: true
+                }
+            });
+        },
+        
+        nossasCustomizacoes: {
+            errorClass: "help-block",
+            highlight: function (element) {
+                $(element).parent().removeClass("has-success");
+                $(element).parent().addClass("has-error");
+            },
+            unhighlight: function (element) {
+                $(element).parent().removeClass("has-error");
+            },
+            success: function (label) {
+                label.parent().addClass("has-success");
+            },
+            messages: {
+                confirmarSenha: {
+                    equalTo: "O campo confirmação de senha deve ser identico ao campo senha."
+                }
+            }
+        }
+    };
+
+    App.Validador.iniciar();
+})();
--- src/cwi-colaai/cwi-colaai-web/src/main/resources/static/js/site/view/usuarioView.js
+++ src/cwi-colaai/cwi-colaai-web/src/main/resources/static/js/site/view/usuarioView.js
@@ -11,64 +11,24 @@
             this.$form = $("#cadastrar-usuario-form");
         },
         vincularEventos: function () {
-
             App.Mascara.telefone(this.$form.find("#telefone"));
-
-            this.$form.validate({
-                errorClass: "help-block",
-                
-                highlight: function (element) {
-                    $(element).parent().removeClass("has-success");
-                    $(element).parent().addClass("has-error");
-                },
-                
-                unhighlight: function (element) {
-                    $(element).parent().removeClass("has-error");
-                },
-                
-                success: function (label) {
-                    label.parent().addClass("has-success");
-                },
-                
-                rules: {
-                    nome: {
-                        required: true,
-                        minlength: 3
-                    },
-                    sobrenome: {
-                        required: true,
-                        minlength: 3
-                    },
-                    telefone: {
-                        required: true,
-                        minlength: 16,
-                        maxlength: 17
-                    },
-                    sexo: {
-                        required: true
-                    },
-                    email: {
-                        required: true,
-                        email: true
-                    },
-                    senha: {
-                        required: true
-                    },
-                    confirmarSenha: {
-                        required: true,
-                        equalTo: "#senha"
-                    },
-                    arquivo: {
-                        required: true
-                    }
-                },
-                messages: {
-                    confirmarSenha: {
-                        equalTo: "O campo confirmação de senha deve ser identico ao campo senha."
-                    }
-                }
-            });
+            this.$form.validate(App.Validador.nossasCustomizacoes);
         }
     };
+    
+    App.LoginUsuarioView = {
+        iniciar: function () {
+            this.buscarElementos();
+            this.vincularEventos();
+        },
+        buscarElementos: function () {
+            this.$form = $("#login-usuario-form");
+        },
+        vincularEventos: function () {
+            this.$form.validate(App.Validador.nossasCustomizacoes);
+        }
+    };
+    
     App.CadastroUsuarioView.iniciar();
+    App.LoginUsuarioView.iniciar();
 })();
