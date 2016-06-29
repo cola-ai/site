811a191c9641c99a3cbcef8563f928887fa52c2f
Diuly Any Moch Barreto <diuly.barreto@cwinet.local>
2016-06-29 14:32:55 -0300

    fix(Entidade): alterar tipo de relacionamento entre Usuario e Token.

--- src/cwi-colaai/cwi-colaai-entity/src/main/java/br/com/cwi/colaai/entity/Token.java
+++ src/cwi-colaai/cwi-colaai-entity/src/main/java/br/com/cwi/colaai/entity/Token.java
@@ -11,12 +11,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -40,8 +40,9 @@ public class Token implements Serializable {
     @Column(name = "VL_TOKEN")
     private String valor;
     
-    @JoinColumn(name = "ID_USUARIO", referencedColumnName = "ID_USUARIO")
-    @OneToOne(optional = false, fetch = FetchType.EAGER)
+    @Basic(optional = false)
+    @ManyToOne
+    @JoinColumn(name = "ID_USUARIO")
     private Usuario usuario;
     
     @Enumerated(EnumType.STRING)
