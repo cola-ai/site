db5d714b2c7ee58b57cbc0a9bfe98b702149a27a
Diuly Barreto <Diuly Barreto>
2016-07-02 20:23:30 -0300

    test(Controller): adicionar testes da controller de acesso.

--- src/cwi-colaai/cwi-colaai-web/src/test/java/br/com/cwi/colaai/web/controllers/AcessControllerTest.java
+++ src/cwi-colaai/cwi-colaai-web/src/test/java/br/com/cwi/colaai/web/controllers/AcessControllerTest.java
@@ -8,24 +8,19 @@ package br.com.cwi.colaai.web.controllers;
 import br.com.cwi.colaai.entity.Usuario;
 import br.com.cwi.colaai.entity.view_model.UsuarioViewModel;
 import br.com.cwi.colaai.service.servicos.UsuarioServico;
-import javax.servlet.http.HttpSession;
-import static org.apache.http.client.methods.RequestBuilder.post;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.doReturn;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
@@ -37,10 +32,7 @@ public class AcessControllerTest {
     
     @Mock
     UsuarioServico usuarioServico;
-    
-    @Mock
-    Usuario usuario;
-    
+        
     @InjectMocks
     AcessController acessController;
     
@@ -48,6 +40,9 @@ public class AcessControllerTest {
     
     @Before
     public void setUp() {
+        UsuarioViewModel user = new UsuarioViewModel();
+        user.setIdUsuario(1l);
+        doReturn(user).when(usuarioServico).buscarPorEmail("email");
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/templates/");
         viewResolver.setSuffix(".html");
@@ -78,11 +73,30 @@ public class AcessControllerTest {
     /**
      * Test of esqueceuSenha method, of class AcessController.
      */
-//    @Test
-//    public void testEsqueceuSenha_Model() throws Exception {
-//        mockMvc.perform(get("/esqueceuSenha"))
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("esqueceuSenha"));
-//    }
+    @Test
+    public void testEsqueceuSenha_Model() throws Exception {
+        mockMvc.perform(get("/esqueceuSenha"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("esqueceuSenha"));
+    }
+    
+    @Test
+    public void testeEsqueceuSenha_UsuarioViewModelValido() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/esqueceuSenha")
+                .param("email", "email"))
+                .andExpect(status().is3xxRedirection()).andExpect(view().name("redirect:login?verifiqueEmail"));
+    }
+    
+    @Test
+    public void testeEsqueceuSenha_UsuarioViewModelNull() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/esqueceuSenha"))
+                .andExpect(status().is3xxRedirection()).andExpect(view().name("redirect:/esqueceuSenha?usuarioNaoEncontrado"));
+    }
 
+    @Test
+    public void testeAlterarSenha_UsuarioValido() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/alterarSenha")
+               .param("idUsuario", "1"))
+               .andExpect(status().is3xxRedirection()).andExpect(view().name("redirect:/login?senhaAlterada"));
+    }
 }
