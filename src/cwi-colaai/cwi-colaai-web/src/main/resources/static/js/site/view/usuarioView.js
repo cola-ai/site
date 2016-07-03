dacdbb494ac7167362377f6db7810aa4aafa4cce
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-07-03 03:07:05 -0300

    feat(Usuario): implementado validacoes nas configurações dos usuarios.

--- src/cwi-colaai/cwi-colaai-web/src/main/resources/static/js/site/view/usuarioView.js
+++ src/cwi-colaai/cwi-colaai-web/src/main/resources/static/js/site/view/usuarioView.js
@@ -15,5 +15,24 @@
         }
     };
     
+    App.ConfiguracoesUsuarioView = {
+        iniciar: function () {
+            this.buscarElementos();
+            this.vincularEventos();
+        },
+        buscarElementos: function () {
+            this.$alterarCadastroform = $("#alterar-cadastro-form");
+            this.$alterarImagemform = $("#alterar-imagem-form");
+        },
+        vincularEventos: function () {
+            App.Mascara.telefone(this.$alterarCadastroform.find("#telefone"));
+            
+            this.$alterarImagemform.find("#file").click(function() {
+                
+            });
+        }
+    };
+    
+    App.ConfiguracoesUsuarioView.iniciar();
     App.CadastroUsuarioView.iniciar();
 })();
