79ce04a29ce312653eca5200b3c190ddb6c70af9
Diuly Any Moch Barreto <diuly.barreto@cwinet.local>
2016-07-04 16:43:07 -0300

    test(UsuarioController): modificar método estático para ser mockado e adicionar teste de alterações de dados cadastrais.

--- src/cwi-colaai/cwi-colaai-security/src/main/java/br/com/cwi/colaai/security/service/SocialUserDetailsService.java
+++ src/cwi-colaai/cwi-colaai-security/src/main/java/br/com/cwi/colaai/security/service/SocialUserDetailsService.java
@@ -5,6 +5,7 @@ import br.com.cwi.colaai.security.enumeration.InformacoesUsuarioAtual;
 import br.com.cwi.colaai.security.enumeration.SocialRoles;
 import br.com.cwi.colaai.service.servicos.UsuarioServico;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -17,16 +18,20 @@ import org.springframework.stereotype.Service;
 public class SocialUserDetailsService implements UserDetailsService {
 
     @Autowired
-    UsuarioServico usuarioServico; 
-    
+    UsuarioServico usuarioServico;
+
     @Override
     public InformacoesUsuarioAtual loadUserByUsername(String username) throws UsernameNotFoundException {
         UsuarioViewModel usuario = usuarioServico.buscarAutorizadoPorEmail(username);
-        
+
         if (usuario == null) {
             throw new UsernameNotFoundException(String.format("Usuario %s não encontrado", username));
-        }        
+        }
         return new InformacoesUsuarioAtual(username, usuario.getSenha(), SocialRoles.valuesToList(), usuario);
+    }
+
+    public InformacoesUsuarioAtual getInformacoesUsuarioAtual() {
+        return (InformacoesUsuarioAtual) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
 }
